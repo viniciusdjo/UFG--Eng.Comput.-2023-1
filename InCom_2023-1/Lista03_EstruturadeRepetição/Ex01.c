33bdf34d5a08fac304f85c3967e5a657b2861c87
Enzo Daniel Marinho Gomes <gomes.enzodani@gmail.com>
2023-06-19 18:04:03 -0300

    Exercícios Lista 3.c
    Exercícios 1 a 2

---
+++ InCom_2023-1/Lista03_EstruturadeRepetição/Ex01.c
@@ -0,0 +1,15 @@
+//1. Escreva um programa que exiba os números de 1 a 10 utilizando um loop for
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    for ( i = 1; i < 11; i++)
+    {
+        printf("\nNumero %d", i);
+    }
+    
+    return 0;
+}
---
+++ InCom_2023-1/Lista03_EstruturadeRepetição/Ex02.c
@@ -0,0 +1,14 @@
+//2. Escreva um programa que exiba os números de 10 a 1 em ordem decrescente utilizando um loop for
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    for ( i = 10; i > 0; i--)
+    {
+        printf("\nNumero %d", i);
+    }
+    
+    return 0;
+}
