a88efaa2f55fc4628772472ea0f2cec70e7532d8
Enzo Daniel Marinho Gomes <gomes.enzodani@gmail.com>
2023-07-27 20:45:21 -0300

    Final Lista 06

--- InCom_2023-1/Faculdade/Lista06_Funções/Ex08.c
+++ InCom_2023-1/Faculdade/Lista06_Funções/Ex08.c
@@ -2,3 +2,30 @@
 Faça um programa que permita entrar com o valor de um produto e o percentual de
 desconto e imprimir o novo valor com base no percentual informado. Para fazer o
 cálculo, implemente uma função*/
+
+#include <stdio.h>
+
+float calculo(float v, float d){
+    int resultado;
+
+    resultado = v - (v*d);
+
+    return resultado;
+}
+
+int main(){
+
+    float valor, desconto;
+
+    //Leitura
+    printf("Digite o valor do produto: ");
+        scanf("%f", &valor);
+    printf("Digite o valor do desconto (em porcentagem): ");
+        scanf("%f", &desconto);
+    desconto = desconto / 100;
+
+    //Chamada da função
+    printf("Novo valor = R$ %2.2f", calculo(valor,desconto));
+
+    return 0;
+}
--- InCom_2023-1/Faculdade/Lista06_Funções/Ex09.c
+++ InCom_2023-1/Faculdade/Lista06_Funções/Ex09.c
@@ -1,3 +1,48 @@
 /*Faça um programa que verifique quantas vezes um número é divisível por outro. A
 função deve receber dois parâmetros, o dividendo e o divisor. Ao ler o divisor, é
 importante verificar se ele é menor que o dividendo. Ao final imprima o resultado.*/
+
+#include <stdio.h>
+
+int contador (int div, int dev){
+    int count = 0;
+
+    while (dev > div || dev == div){
+        if(dev%div != 0){ //Caso o numero n seja divisivel, a função vai retornar quantas vezes ele foi
+            return count;
+        }
+        else{
+            dev = dev / div; //A cada divisão, é contabilizada em um contador ate que
+                             //dividendo seja menor que o divisor ou não possa mais ser dividido
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int main (){
+    
+    int divisor, dividendo;
+
+    //Para que o programa entre no while sem problemas, eu inicializei dessa forma
+    divisor = 2;
+    dividendo = 1;
+
+    //O programa vai se manter ai até que a condição seja atendida
+    while (divisor > dividendo){
+
+    //Leitura 
+    printf("Digite o dividendo (o numero a ser dividido): ");
+        scanf("%d", &dividendo);
+    printf("Obs: O programa vai voltar a requisao caso divisor > dividendo");
+    printf("\nDigite o divisor (o numero que ira dividir): ");
+        scanf("%d", &divisor);
+
+    }
+    //Chamada da função
+    printf("O %d é %d vezes divisível por %d", dividendo, contador(divisor, dividendo), divisor);
+
+    
+    return 0;
+}
