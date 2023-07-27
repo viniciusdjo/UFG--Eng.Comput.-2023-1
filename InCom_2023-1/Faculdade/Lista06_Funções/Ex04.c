615d4958137c6bbdc0d30baf5f505cf92e6d95f2
Enzo Daniel Marinho Gomes <gomes.enzodani@gmail.com>
2023-07-27 18:24:07 -0300

    Update Lista Função

--- InCom_2023-1/Faculdade/Lista06_Funções/Ex04.c
+++ InCom_2023-1/Faculdade/Lista06_Funções/Ex04.c
@@ -1,2 +1,29 @@
 /*Faça um programa que leia o saldo e o reajuste (%) de uma aplicação financeira e
 imprima o novo saldo após o reajuste. O cálculo deve ser feito por uma função*/
+#include <stdio.h>
+
+//Função de cálculo
+//Como é uma aplicação, é melhor usar float
+float calcularNovoSaldo(float saldo, float reajuste) {
+    float novoSaldo = saldo + (saldo * reajuste / 100);
+    return novoSaldo;
+}
+
+int main() {
+    float saldo, reajuste;
+
+    // Leitura do saldo e do percentual de reajuste
+    printf("Digite o saldo da aplicação: ");
+    scanf("%f", &saldo);
+
+    printf("Digite o percentual de reajuste: ");
+    scanf("%f", &reajuste);
+
+    // Cálculo do novo saldo usando a função calcularNovoSaldo
+    float novoSaldo = calcularNovoSaldo(saldo, reajuste);
+
+    // Impressão do novo saldo após o reajuste
+    printf("Novo saldo após o reajuste: %.2f\n", novoSaldo);
+
+    return 0;
+}
--- InCom_2023-1/Faculdade/Lista06_Funções/Ex10.c
+++ InCom_2023-1/Faculdade/Lista06_Funções/Ex10.c
@@ -3,3 +3,37 @@ programa deve, por meio de função, retirar todas as ocorrências do caractere
 informado na mensagem colocando * em seu lugar. A função deve também retornar
 o total de caracteres retirados. Ao final, o programa deve imprimir a frase ajustada e
 a quantidade de caracteres substituídos*/
+#include <stdio.h>
+
+//Função de substiuição
+int substuir(char *mes, char alvo) {
+    int count = 0;
+    for (int i = 0; mes[i] != '\0'; i++) {
+        if (mes[i] == alvo) {
+            mes[i] = '*';
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    char mes[100];
+    char alvo;
+
+    // Leitura da mensagem e caractere alvo
+    printf("Digite a mensagem: ");
+    fgets(mes, sizeof(mes), stdin);
+
+    printf("Digite o caractere a ser substituído: ");
+    scanf("%c", &alvo);
+
+    // Remover o caractere alvo e obter a quantidade de substituições
+    int contador = substuir(mes, alvo);
+
+    // Imprimir a mensagem ajustada e a quantidade de substituições
+    printf("Mensagem ajustada: %s", mes);
+    printf("Total de caracteres substituídos: %d\n", contador);
+
+    return 0;
+}
