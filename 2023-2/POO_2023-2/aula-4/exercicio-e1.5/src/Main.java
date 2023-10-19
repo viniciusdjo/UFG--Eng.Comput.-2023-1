2b095998d244d136fb2adf9a1510e8c303f9ec3a
enzzodani <gomes.enzodani@gmail.com>
2023-10-19 13:30:41 -0300

    Exercício e1.5 poo (versão 3)

---
+++ 2023-2/POO_2023-2/aula-4/exercicio-e1.5/src/Main.java
@@ -0,0 +1,22 @@
+import model.GeometricForms;
+import subclasses.Circle;
+import subclasses.Rectangle;
+import subclasses.Triangle;
+
+import static util.SpaceCalculator.calculateTotalArea;
+
+public class Main {
+    public static void main(String[] args) {
+        GeometricForms[] forms = new GeometricForms[3];
+
+        GeometricForms Retangulo = new Rectangle(10, 15);
+        GeometricForms Circulo = new Circle(5);
+        GeometricForms Triangulo = new Triangle(10, 5);
+
+        forms[0] = Retangulo;
+        forms [1] = Circulo;
+        forms [2] = Triangulo;
+
+        System.out.println(calculateTotalArea(forms));
+    }
+}
--- 2023-2/POO_2023-2/aula-4/exercicio-e1.5/src/util/SpaceCalculator.java
+++ 2023-2/POO_2023-2/aula-4/exercicio-e1.5/src/util/SpaceCalculator.java
@@ -1,32 +1,13 @@
 package util;
 
 import model.GeometricForms;
-import subclasses.Circle;
-import subclasses.Rectangle;
-import subclasses.Triangle;
-
-import java.util.Arrays;
 
 public class SpaceCalculator {
     public static double calculateTotalArea(GeometricForms[] form){
         double totalArea = 0.0;
         for (GeometricForms forms : form) {
-            totalArea += forms.calculateArea();;
+            totalArea += forms.calculateArea();
         }
         return totalArea;
-    }
-
-    public static void main(String[] args) {
-        GeometricForms[] forms = new GeometricForms[3];
-
-        GeometricForms Retangulo = new Rectangle(10, 15);
-        GeometricForms Circulo = new Circle(5);
-        GeometricForms Triangulo = new Triangle(10, 5);
-
-        forms[0] = Retangulo;
-        forms [1] = Circulo;
-        forms [2] = Triangulo;
-
-        System.out.println(calculateTotalArea(forms));
     }
 }
