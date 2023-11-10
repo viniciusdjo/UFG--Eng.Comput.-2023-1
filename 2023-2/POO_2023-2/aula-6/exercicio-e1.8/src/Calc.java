e14ebb328293968e92d97bcb120cd5f8d6c77597
enzzodani <gomes.enzodani@gmail.com>
2023-11-10 15:29:31 -0300

    Java Calc  Swing version 3 Toda a lógica da calculadora foi feita, apenas falta tratar das exceções

--- 2023-2/POO_2023-2/aula-6/exercicio-e1.8/src/Calc.java
+++ 2023-2/POO_2023-2/aula-6/exercicio-e1.8/src/Calc.java
@@ -9,6 +9,8 @@ import javax.swing.*;
 
 public class Calc {
     private JTextField txtNum1;
+    private JTextField txtNum2;
+    private JComboBox<Object> op;
     public Calc() {
         prepareGUI();
     }
@@ -19,7 +21,7 @@ public class Calc {
     private void prepareGUI() {
 //        JFrame
         JFrame mainFrame = new JFrame();
-        mainFrame.setBounds(100, 100, 400, 400);
+        mainFrame.setBounds(100, 100, 800, 400);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setLayout(new FlowLayout());
             /*
@@ -34,14 +36,14 @@ public class Calc {
 
 //        JButton
         JButton btnCalc = new JButton("Calcule");
-        btnCalc.setActionCommand("Action");
+        btnCalc.setActionCommand("Calc");
 
 //        JText Num1 and Num2
         txtNum1 = new JTextField(30);
-        JTextField txtNum2 = new JTextField(30);
+        txtNum2 = new JTextField(30);
 
 //        JComboBox
-        JComboBox<Object> op = new JComboBox<>();
+        op = new JComboBox<>();
         for (String s : Arrays.asList("+", "-", "*", "/")) {
             op.addItem(s);
         }
@@ -62,12 +64,33 @@ public class Calc {
         @Override
         public void actionPerformed(ActionEvent e) {
             String command = e.getActionCommand();
+            switch (command) {
+                case "Calc":
+                    if (txtNum1.getText() != null && txtNum2.getText() != null && op.getSelectedItem() != null) {
+                        Integer num1 = Integer.parseInt(txtNum1.getText());
+                        Integer num2 = Integer.parseInt(txtNum2.getText());
 
-            if (command.equals("Action")) {
-                System.out.println("My first event");
-                JOptionPane.showMessageDialog(null, txtNum1.getText());
-            } else {
-                System.out.println("Invalid Action");
+                        String opSelected = (String) op.getSelectedItem();
+
+                        switch (opSelected) {
+                            case "+":
+                                JOptionPane.showMessageDialog(null, num1 + num2);
+                                break;
+                            case "-":
+                                JOptionPane.showMessageDialog(null, num1 - num2);
+                                break;
+                            case "*":
+                                JOptionPane.showMessageDialog(null, num1 * num2);
+                                break;
+                            case "/":
+                                JOptionPane.showMessageDialog(null, num1 / num2);
+                                break;
+                            default:
+                                System.out.println("Invalid Action");
+                        }
+                    }
+                    break;
+                default:
             }
         }
 
