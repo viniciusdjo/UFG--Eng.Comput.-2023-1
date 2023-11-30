f97bee69ffc138582f9f3188d44209759f3e964f
enzzodani <gomes.enzodani@gmail.com>
2023-11-30 13:19:19 -0300

    Threads class, daemon lesson

--- 2023-2/POO_2023-2/Java-class/threads/threads/src/Main.java
+++ 2023-2/POO_2023-2/Java-class/threads/threads/src/Main.java
@@ -7,9 +7,13 @@ public class Main {
         MyRunnable runnable = new MyRunnable();
         Thread thread2 = new Thread(runnable);
 
+//        Daemon
+        thread1.setDaemon(true);
+        thread2.setDaemon(false);
+
 //        Execution
         thread1.start();
-        thread1.join(3000);
+        //thread1.join(3000);
         thread2.start();
 
         System.out.println(1/0);
